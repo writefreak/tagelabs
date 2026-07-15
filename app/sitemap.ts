105d2c4e58497aa5e644544ff5749a4fc0339391
Writefreak <iscribbledit@gmail.com>
2026-07-15 13:55:27 +0100

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -30,7 +30,7 @@ export default async function sitemap() {
       priority: 1,
     },
     {
-      url: `${baseUrl}/blog`,
+      url: `${baseUrl}/blogs`,
       lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.8,
