d952ce2adcf5448a8ab35a983ceec283cf6fcb5c
Writefreak <iscribbledit@gmail.com>
2026-07-29 04:34:24 +0100

    update about

--- components/About.tsx
+++ components/About.tsx
@@ -114,7 +114,7 @@ function ParallaxImage({ className }: { className: string }) {
       className={className}
     >
       <motion.img
-        src="/code.jpg"
+        src="/type.png"
         alt="TageLabs studio"
         style={{ y: imageY }}
         className="absolute left-0 -top-[15%] h-[130%] w-full object-cover"
